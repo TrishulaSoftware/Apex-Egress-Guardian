594886c255c287cf005fcd2c3d92d1de565b8e0f
Trishula Software <engineering@trishulasoftware.com>
2026-04-27 16:28:44 -0400

    fix(ci): Root-cause remediation — add setup.py for Cython build, timeout for daemon scripts, mkdir telemetry

---
+++ setup.py
@@ -0,0 +1,8 @@
+﻿from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name="ApexEgressGuardian",
+    ext_modules=cythonize("pinner.py", compiler_directives={'language_level': "3"}),
+    zip_safe=False,
+)
